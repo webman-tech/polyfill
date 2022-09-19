897143c6b44ae39f814cf4a83a2253f18add5db1
kriss <462679766@qq.com>
2022-09-19 09:47:54 +0800

    feature: symfony/http-foundation > 6 manual
    fix #1

--- src/Traits/SymfonyRequestWrapper.php
+++ src/Traits/SymfonyRequestWrapper.php
@@ -32,10 +32,12 @@ trait SymfonyRequestWrapper
     }
 
     /**
+     * 暂时移除，因为 symfony/http-foundation > 6.0 版本后该方法定义了类型返回，小于 6 的没有，暂时无法兼容
+     * 后期升级到 8.0 支持后打开
      * @inheritDoc
      */
-    public static function createFromGlobals()
+    /*public static function createFromGlobals()
     {
         throw new \InvalidArgumentException('Not support');
-    }
+    }*/
 }
