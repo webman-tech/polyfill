67f85a37d7dc9576d2e593e445558bb1aa35e2e9
kriss <462679766@qq.com>
2022-10-19 17:36:43 +0800

    feature: update config

--- src/config/plugin/webman-tech/polyfill/app.php
+++ src/config/plugin/webman-tech/polyfill/app.php
@@ -11,14 +11,29 @@ return [
          * 如果用到 Laravel UploadedFile 中的 store 或 storeAs 相关方法，需要提供 filesystemFactory 实现
          */
         'filesystem' => function (): FilesystemFactory {
+            if (class_exists(\WebmanTech\LaravelFilesystem\Facades\Storage::class)) {
+                return \WebmanTech\LaravelFilesystem\Facades\Storage::instance();
+            }
+            if (!Container::has(FilesystemFactory::class)) {
+                throw new InvalidArgumentException('请先配置 FilesystemFactory 实现');
+            }
             return Container::get(FilesystemFactory::class);
         },
         /**
          * 如果用到 Laravel Request 中的 validate，需要提供 validationFactory 实现
          */
         'validation' => function (): ValidationFactory {
+            if (class_exists(\WebmanTech\LaravelValidation\Facades\Validator::class)) {
+                return \WebmanTech\LaravelValidation\Facades\Validator::instance();
+            }
             if (function_exists('validator')) {
-                return validator();
+                $validator = validator();
+                if ($validator instanceof ValidationFactory) {
+                    return $validator;
+                }
+            }
+            if (!Container::has(ValidationFactory::class)) {
+                throw new InvalidArgumentException('请先配置 ValidationFactory 实现');
             }
             return Container::get(ValidationFactory::class);
         }
